a485d3cd67cf669986425c269e938c28c14e777f
jorozcove <jorozcove@unal.edu.co>
2022-02-15 18:24:15 -0500

    First Commit

---
+++ content.js
@@ -0,0 +1,157 @@
+//date functions
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0')
+  }
+  
+  function formatDate(date) {
+    return (
+      [
+        date.getFullYear(),
+        padTo2Digits(date.getMonth() + 1),
+        padTo2Digits(date.getDate()),
+      ].join('-') +
+      ' ' +
+      [
+        padTo2Digits(date.getHours()),
+        padTo2Digits(date.getMinutes())
+      ].join(':')
+    )
+  }
+  
+  // 2021-10-24 16:21:23 (yyyy-mm-dd hh:mm)
+  // console.log(formatDate(new Date()))
+  
+  // 2025-05-04 05:24:07 (yyyy-mm-dd hh:mm)
+  //   console.log(formatDate(new Date('May 04, 2025 05:24:07')))
+
+//get a list of the current courses
+function getCoursesList(){
+    var preRows = document.getElementsByClassName("af_table_data-row")
+    var rows = []
+    for(var preRow of preRows){
+        var row = preRow.getElementsByClassName("af_column_data-container")
+        rows.push(row)
+    }
+    var i = 1
+    for(var row of rows){
+        console.log(`[${i}]\n----------------------`)
+        for(var data of row){
+            console.log(data.textContent)
+        }
+        console.log("----------------------")
+        i+=1
+    }
+}
+    
+//get all the info of the current course and return it into a json object
+function getCourseData(){
+
+    var courseObj = {}
+    var courseName = document.getElementsByTagName("h2")[0].textContent
+    var groupList = []
+
+    courseObj["nombreAsignatura"] = courseName
+    courseObj["cuposDisponibles"] = 0
+    courseObj["fechaObtencion"] = formatDate(new Date())
+    courseObj["grupos"] = groupList
+
+    var groups = document.querySelectorAll(".borde.salto:not(.ficha-docente)") //get groups and avoid "Contenido de asignatura"
+    //groups = [].slice.call(groups).slice(0,-1) // TEMPORAL FIX (avoid prerrequisites)
+
+    for(var group of groups){
+
+        if(group.getElementsByClassName("margin-t")[1] == undefined){break} //avoid prerrequisites or correquisites info
+        
+        var groupObj = {}
+
+        groupObj["nombreGrupo"] = group.getElementsByClassName("af_showDetailHeader_title-text0 ")[0].textContent
+
+        var groupData = group.getElementsByClassName("margin-t")[1].children
+
+        groupObj["profesor"] = groupData[0].textContent.split(": ")[1]
+
+        groupObj["facultad"] = groupData[1].textContent.split(": ")[1]
+
+        groupObj["nombre"] = courseName
+
+        var shedules = []
+        var sheduleData = []
+        var shedule = {}
+
+        var sData = groupData[2].getElementsByClassName("af_panelGroupLayout")[0]
+        if(sData.childElementCount>0){
+            sData = [].slice.call(sData.children[0].children).slice(2)
+            for(let s of sData){
+                sheduleData = s.children[0].textContent.split(" ")
+
+                shedule["dia"] = sheduleData[0]
+                shedule["desde"] = sheduleData[2]
+                shedule["hasta"] = sheduleData[4].replace(".","")
+
+                shedules.push(shedule)
+
+                shedule = {}
+            }
+    
+        }
+
+        groupObj["horarios"] = shedules
+
+        groupObj["duracion"] = groupData[3].textContent.split(": ")[1]
+
+        groupObj["jornada"] = groupData[4].textContent.split(": ")[1]
+
+        if(groupData[5]==undefined){
+            groupObj["cupos"] = "NaN"
+        }else{
+            groupObj["cupos"] = parseInt(groupData[5].textContent.split(": ")[1])
+            courseObj["cuposDisponibles"] += parseInt(groupData[5].textContent.split(": ")[1])
+        }
+
+        //add group to course
+        courseObj["grupos"].push(groupObj)
+
+        groupObj = {}
+
+    }
+    return courseObj
+
+}
+
+// check if the user is on course page (this is a bad way to do this, but is fine for now)
+function isCoursePage(){
+    var url = document.URL
+    if(url.startsWith("https://sia.unal.edu.co/")){ //&& url.endsWith("AC_CatalogoAsignaturas")
+        try{
+            if(document.getElementsByClassName("ocu-titulo")[0].textContent=="Información de la asignatura"){
+                return true
+            }
+        }catch(e){
+            return false
+        }
+        
+    }
+    return false
+}
+
+// add the listeners that receive events from popup
+chrome.runtime.onMessage.addListener(function (request){   
+    //console.log(request)
+    switch(request.operation){
+        case "LOAD": {   
+            //console.log("LOAD PRESSED")
+            document.dispatchEvent(new CustomEvent('coursesEvent', {detail: request.data}))
+            break
+        }
+        case "SAVE": {
+            if(isCoursePage()){
+                var data = getCourseData()
+                //send data back to popup.js
+                chrome.runtime.sendMessage(data)
+            }else{
+                console.log("No se encuentra en la pagina de una asignatura")
+            }
+            break
+        }
+    }
+})
---
+++ popup.js
@@ -0,0 +1,183 @@
+// fetch the items stored in entension localStorage and load into the popup
+function fetchItems(){
+
+    const itemsList = document.querySelector("div#todo-container")
+    itemsList.innerHTML = ""
+    var newInnerHtml = " "
+
+    try{
+        var itemsStorage = localStorage.getItem("todo-items")
+        var itemsArr = JSON.parse(itemsStorage)
+        var itemStatus;
+
+        for(var i = 0; i<itemsArr.length; i++){
+
+            itemStatus = itemsArr[i].status ? "checked" : ""
+            newInnerHtml += `
+            <div class="col col-12 p-2 todo-item" todo-id="${i}">
+            <div class="input-group">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <input class = "itemCheck" type="checkbox" aria-label="Checkbox for following text input" ${itemStatus} >
+              </div>
+            </div>
+            <input type="text" readonly class="form-control" aria-label="Text input with checkbox"
+              value="${itemsArr[i].nombreAsignatura} [${itemsArr[i].fechaObtencion}]">
+            <div class="input-group-append">
+            <button todo-id="${i}" class="itemDownload btn btn-outline-secondary bg-info text-white" type="button"
+                id="button-addon1 "><i class="fa fa-download"></i></button>
+
+              <button todo-id="${i}" class="itemDelete btn btn-outline-secondary bg-danger text-white" type="button"
+                id="button-addon2 ">X</button>
+            </div>
+            </div>
+            </div>
+            `
+        }
+
+        itemsList.innerHTML = newInnerHtml;
+        
+        var iList = document.querySelector("div#todo-container").children
+
+        //add specific functionality for each item
+        for(var i = 0; i<iList.length; i++){
+            // save everytime checked something
+            iList[i].querySelector(".itemCheck").addEventListener("click", (function(i){
+                return function(){
+                    changeCheckValue(i)
+                }
+            })(i))
+
+            // add download function
+            iList[i].querySelector(".itemDownload").addEventListener("click", (function(i){
+                return function(){
+                    itemDownload(i)
+                }
+            })(i))
+
+            // add delete function
+            iList[i].querySelector(".itemDelete").addEventListener("click", (function(i){
+                return function(){
+                    itemDelete(i)
+                }
+            })(i))
+        }
+        
+        
+    }catch(e){
+        console.error(e)
+    }
+    
+
+}
+
+//save the given object to the storage
+function saveItems(obj){
+    var string = JSON.stringify(obj)
+    localStorage.setItem("todo-items", string)
+}
+
+//change the check value and save the new one by index
+function changeCheckValue(index){
+    var itemsStorage = localStorage.getItem("todo-items")
+    var itemsArr = JSON.parse(itemsStorage)
+    itemsArr[index].status = !itemsArr[index].status
+    saveItems(itemsArr)
+}
+
+//delete the item from the popup and the stoage by index
+function itemDelete(index){
+    var itemsStorage = localStorage.getItem("todo-items")
+    var itemsArr = JSON.parse(itemsStorage)
+
+    itemsArr.splice(index, 1)
+    saveItems(itemsArr)
+
+    document.querySelector('div#todo-container .todo-item[todo-id="'+index+'"]').remove()
+}
+
+//donwload the item from the stoage by index
+function itemDownload(index){
+    var itemsStorage = localStorage.getItem("todo-items")
+    var itemsArr = JSON.parse(itemsStorage)
+    downloadJSON(itemsArr[index],itemsArr[index].nombreAsignatura)
+}
+
+//download a JSON object
+function downloadJSON(obj,name){
+    obj = JSON.stringify(obj,null,4);
+    var vLink = document.createElement('a'),
+    vBlob = new Blob([obj], {type: "octet/stream"}),
+    vName = name+".json",
+    vUrl = window.URL.createObjectURL(vBlob);
+    vLink.setAttribute('href', vUrl);
+    vLink.setAttribute('download', vName );
+    vLink.click();
+}
+
+//get all the checkef items 
+function getChkItems(){
+    var itemsStorage = localStorage.getItem("todo-items")
+    var itemsArr = JSON.parse(itemsStorage)
+    var checkedItems = []
+    for(var item of itemsArr){
+        if(item.status){
+            checkedItems.push(item)
+        }
+    }
+    return checkedItems
+}
+
+function main(){
+    //set empty list in local storage if it don't exist
+    if(localStorage.getItem('todo-items')==undefined) localStorage.setItem('todo-items','[]')
+
+    //add listener to send actual local storage to content.js to perform load operation
+    document.querySelector("#btn1").addEventListener('click', function(){
+        var data = localStorage.getItem("todo-items")
+        chrome.tabs.query({currentWindow: true, active: true},
+        function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,{
+                "operation":"LOAD",
+                "data": data
+            })
+        })
+
+    }, false)
+
+    //send save operation msg to content.js
+    document.querySelector("#btn2").addEventListener('click', function(){
+        chrome.tabs.query({currentWindow: true, active: true},
+            function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id,{
+                    "operation":"SAVE",
+                    "data": ""
+                })
+            })
+    }, false)
+
+    // add listener to get course data from content.js
+    chrome.runtime.onMessage.addListener(function (request){
+        var itemsStorage = localStorage.getItem("todo-items")
+        var itemsArr = JSON.parse(itemsStorage)
+        request.status = false
+        console.log(request)
+        itemsArr.push(request)
+        saveItems(itemsArr)
+        fetchItems()
+    })
+
+    // set functionality for download all button
+    const dwnAllBtn = document.querySelector('#downloadAllBtn')
+    dwnAllBtn.addEventListener("click", function(){
+        var checkedItems = getChkItems()
+        if(checkedItems.length>0){
+            downloadJSON(checkedItems,"Asignaturas")
+        }  
+    })
+
+    //fetch the items when the popup is opened
+    fetchItems()
+}
+
+main()
